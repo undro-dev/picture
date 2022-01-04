837be1b1a963ffd4d79fb13d34fb645d1127da67
Vitaly Undro <vitalyundro@gmail.com>
2022-01-04 21:03:58 +0300

    feat: add validation for russian language

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import modals from './modules/modals';
 import sliders from './modules/sliders';
 import forms from './modules/forms';
 import mask from './modules/mask';
+import checkTextInputs from './modules/checkTextInputs';
 
 window.addEventListener('DOMContentLoaded', () => {
   modals();
@@ -9,4 +10,6 @@ window.addEventListener('DOMContentLoaded', () => {
   sliders('.main-slider-item', 'vertical');
   forms();
   mask('[name="phone"]');
+  checkTextInputs('[name="name"]');
+  checkTextInputs('[name="message"]');
 });
